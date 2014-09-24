63e30395fa5fc9cb2c482ff0b8bbba8846345844
Rhea <rhealauzon@gmail.com>
2014-09-24 16:52:57 -0700

    maps v2

--- app/src/main/java/ca/bcit/hometown/hometownodyssey/Map.java
+++ app/src/main/java/ca/bcit/hometown/hometownodyssey/Map.java
@@ -11,7 +11,9 @@ import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class Map extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks,
@@ -26,24 +28,38 @@ public class Map extends FragmentActivity implements GooglePlayServicesClient.Co
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        locationClient = new LocationClient(this, this, this);
-
+        //Create GPS object
         gps = new GPSTracker(Map.this);
 
+        //Check if GPS is up and running
         if(gps.canGetLocation()){
 
-            double latitude = gps.getLatitude();
-            double longitude = gps.getLongitude();
 
+
+            //Create the maps object and initalize it
             GoogleMap map = ((MapFragment) getFragmentManager()
                     .findFragmentById(R.id.map)).getMap();
 
             map.setMyLocationEnabled(true);
+
+            //Get current latitude and logitude
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
             LatLng current = new LatLng(latitude, longitude);
+
+            //Move the camera to the specified location level 17
+            // (note: 2 is min zoom and 21 is max. Anything above and below
+            // Auto becomes the min or max.
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 17));
+            map.addMarker(new MarkerOptions().position(new LatLng(latitude + 0.003, longitude + 0.003)).title("Hello!").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            map.addMarker(new MarkerOptions().position(new LatLng(latitude + 0.0001, longitude + 0.001)).title("Derp").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
 
             Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-        }else{
+
+        }
+        else
+        {
             // can't get location
             // GPS or Network is not enabled
             // Ask user to enable GPS/network in settings
@@ -61,16 +77,6 @@ public class Map extends FragmentActivity implements GooglePlayServicesClient.Co
     public void onConnected(Bundle dataBundle) {
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-
-        locationClient.connect();
-        currentLocation = locationClient.getLastLocation();
-
-        GoogleMap map = ((MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
-
-        double lat = currentLocation.getLatitude();
-        double longi = currentLocation.getLongitude();
-
 
 
     }
