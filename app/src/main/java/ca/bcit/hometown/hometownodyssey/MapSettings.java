472ee8dc16e4e08281b7bd15ce38f4d59e320dc0
Chris Klassen <ca.klassen95@gmail.com>
2014-09-25 19:57:04 -0700

    Created MapSettings class

---
+++ app/src/main/java/ca/bcit/hometown/hometownodyssey/MapSettings.java
@@ -0,0 +1,10 @@
+package ca.bcit.hometown.hometownodyssey;
+
+/**
+ * Created by Chris on 2014-09-25.
+ *
+ * This class handles the settings for the map screen.
+ */
+public class MapSettings {
+    
+}
