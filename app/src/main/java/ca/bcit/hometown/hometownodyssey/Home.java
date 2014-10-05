1941893f5710a8e58e46cece61410884b9997a6c
Chris Klassen <ca.klassen95@gmail.com>
2014-10-05 12:53:46 -0700

    Added colours to the home screen

--- app/src/main/java/ca/bcit/hometown/hometownodyssey/Home.java
+++ app/src/main/java/ca/bcit/hometown/hometownodyssey/Home.java
@@ -13,7 +13,6 @@ public class Home extends Activity implements OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
     }
