107ad8075e519ced47e05b059627e5bfb05ab171
Rhea <rhealauzon@gmail.com>
2014-11-02 15:26:13 -0800

    Basic Inventory UI in place

--- app/src/main/java/ca/bcit/hometown/hometownodyssey/Inventory.java
+++ app/src/main/java/ca/bcit/hometown/hometownodyssey/Inventory.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class Inventory extends Activity {
@@ -12,6 +16,41 @@ public class Inventory extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        //Pull equipped items from person class and change the source
+        ImageButton headButton = (ImageButton) findViewById(R.id.headInv);
+        ImageButton bodyButton = (ImageButton) findViewById(R.id.bodyInv);
+        ImageButton legButton = (ImageButton) findViewById(R.id.legInv);
+        ImageButton footButton = (ImageButton) findViewById(R.id.footInv);
+
+        //Change teh source
+        legButton.setImageResource(R.drawable.ho_icon_skeletonpants);
+
+        //Load head items by default into the listview
+        //Probably get an array list of drawable id's from database?
+        ImageView image = new ImageView(this);
+        image.setBackgroundResource(R.drawable.ho_icon_jackolantern);
+        image.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        ImageView image2 = new ImageView(this);
+        image2.setBackgroundResource(R.drawable.ho_icon_sandal);
+        image2.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        ImageView image3 = new ImageView(this);
+        image3.setBackgroundResource(R.drawable.ho_icon_skeletonpants);
+        image3.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        ImageView image4 = new ImageView(this);
+        image4.setBackgroundResource(R.drawable.ho_icon_jackolantern);
+        image4.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.scrollingInv);
+        ll.addView(image);
+        ll.addView(image2);
+        ll.addView(image3);
+        ll.addView(image4);
+
     }
 
 
@@ -33,4 +72,28 @@ public class Inventory extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void loadHeadItems(View v)
+    {
+      //Empty linear layout
+      //Pull all obtained head items from DB
+      //Populate list
+
+    }
+
+    public void loadBodyItems(View v)
+    {
+
+    }
+
+    public void loadLegItems(View v)
+    {
+
+    }
+
+    public void loadFootItems(View v)
+    {
+
+    }
+
 }
