87ab25c41f4de53107dbfdf2b94d0a03da2cd49b
Chris Klassen <ca.klassen95@gmail.com>
2014-11-20 11:55:03 -0800

    Added basic Settings page

--- app/src/main/java/ca/bcit/hometown/hometownodyssey/Settings.java
+++ app/src/main/java/ca/bcit/hometown/hometownodyssey/Settings.java
@@ -1,12 +1,18 @@
 package ca.bcit.hometown.hometownodyssey;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
-public class Settings extends Activity {
+import ca.bcit.hometown.hometownodyssey.AdventureFragment.OnFragmentInteractionListener;
+
+public class Settings extends Activity implements OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,26 @@ public class Settings extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    public void clearData( View view ) {
+        DatabaseHelper db = DatabaseHelper.getInstance(this);
+
+        db.deleteAllTreasures();
+        db.deleteInventory();
+
+        Player player = new Player("TEMP");
+
+        db.savePlayerData( player );
+
+        FragmentManager fm = getFragmentManager();
+        AdventureFragment ad = (AdventureFragment) fm.findFragmentById( R.id.adventure_fragment );
+        ad.updateCharacter();
+
+        Toast.makeText( this, "Data cleared!", Toast.LENGTH_SHORT ).show();
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+        // Does nothing currently
     }
 }
